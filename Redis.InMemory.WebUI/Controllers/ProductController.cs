855803308ec20b8a5811136c19c773963889c6e9
lbilinmis <leventamed@gmail.com>
2023-06-11 14:50:01 +0300

    SlidingExpiration ve AbsoluteExpiration kullanımları

--- Redis.InMemory.WebUI/Controllers/ProductController.cs
+++ Redis.InMemory.WebUI/Controllers/ProductController.cs
@@ -15,29 +15,44 @@ namespace Redis.InMemory.WebUI.Controllers
 
         public IActionResult Index()
         {
-            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            //if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
 
             // ya da 
 
-            if (_memoryCache.TryGetValue("zaman", out string zamanCache))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            //if (_memoryCache.TryGetValue("zaman", out string zamanCache))
+            //{
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            //options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+
+
+            // aşağıdaki kod blogığ 10 sn ye de tekrar okuma işlemi yapılıp okunduğunda
+            //cache den okumaya devam edecel her istek 10 sn kadar cache de tutulmasını sağlar
+
+            options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+            //}
 
             return View();
         }
 
         public IActionResult Show()
         {
-            _memoryCache.GetOrCreate<string>("zaman", entry =>
-            {
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("zaman", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
 
-            ViewBag.Cache_Zaman = _memoryCache.Get<string>("zaman");
+            //ViewBag.Cache_Zaman = _memoryCache.Get<string>("zaman");
+
+
+
+            _memoryCache.TryGetValue("zaman", out string zamanCache);
+
+            ViewBag.Cache_Zaman = zamanCache;
             return View();
         }
     }
